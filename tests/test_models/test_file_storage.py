3d8b9ce16a3065c095400d01b58e006c4d4eb964
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-11-13 21:41:53 +0000

    adding provisional unittest

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is the test_file_storage module
+for testing the file_storage class
+"""
+
+
+import unittest
+from ...models.engine.file_storage import BaseModel
+
+
+class TestFileStorageClass(unittest.TestCase):
+    """ This is the TestFileStorageClass class """
+    pass
